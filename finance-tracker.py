ddb7ae28fed2172265d76abf6dd720d38e896252
Brendan M Whitney <bwhitne1@uvm.edu>
2018-10-01 10:48:47 -0400

    Added income widget. Should combine income and expense widget because code is similar

--- finance-tracker.py
+++ finance-tracker.py
@@ -4,7 +4,7 @@
 # created on: 09-30-2018
 # filename: finance-tracker.py
 # author: brendan
-# last modified: 10-01-2018 10:41
+# last modified: 10-01-2018 10:47
 #-------------------------------------------------------------------------------
 """
 An interactive widget for tracking my finances. Update this feature list as the
@@ -24,7 +24,7 @@ def makeentry(parent, caption, row, width=None, **options):
 
 
 def expense_widget():
-    def loginfo():
+    def logexpense():
         expense = line1.get()
         line1.delete(0,tk.END)
         reason = line2.get()
@@ -37,15 +37,34 @@ def expense_widget():
     line1 = makeentry(expense, 'Expense:', 0)
     line2 = makeentry(expense, 'Reason:', 1)
     line3 = makeentry(expense, 'Method:', 2)
-    log = tk.Button(expense, text='log', width=10, command=loginfo)
+    log = tk.Button(expense, text='log', width=10, command=logexpense)
     log.grid(row=3, columnspan=2)
     close = tk.Button(expense, text='close', width=10, command=expense.destroy)
     close.grid(row=4, columnspan=2)
 
+def income_widget():
+    def logincome():
+        income = line1.get()
+        line1.delete(0, tk.END)
+        reason = line2.get()
+        line2.delete(0,tk.END)
+        method = line3.get()
+        line3.delete(0,tk.END)
+        print(income, reason, method)
 
-income = tk.Button(master, text='income', width=10)
-income.pack()
-expense = tk.Button(master, text='expense', width=10, command=expense_widget)
-expense.pack()
+    income = tk.Tk()
+    line1 = makeentry(income, 'Income:', 0)
+    line2 = makeentry(income, 'Reason:', 1)
+    line3 = makeentry(income, 'Method:', 2)
+    log = tk.Button(income, text='log', width=10, command=logincome)
+    log.grid(row=3, columnspan=2)
+    close = tk.Button(income, text='close', width=10, command=income.destroy)
+    close.grid(row=4, columnspan=2)
+    
+
+
+tk.Button(master, text='income', width=10, command=income_widget).pack()
+tk.Button(master, text='expense', width=10, command=expense_widget).pack()
+tk.Button(master, text='close', width=10, command=master.destroy).pack()
 
 tk.mainloop()
