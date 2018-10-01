3b23fb74f44b8b1be0f24d86bf21af6e97b81440
Brendan M Whitney <bwhitne1@uvm.edu>
2018-09-30 20:21:47 -0400

    Created the python file

---
+++ finance-tracker.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+#-------------------------------------------------------------------------------
+# created on: 09-30-2018
+# filename: finance-tracker.py
+# author: brendan
+# last modified: 09-30-2018 20:19
+#-------------------------------------------------------------------------------
+"""
+An interactive widget for tracking my finances. Update this feature list as the
+project progesses
+"""
+import pandas as pd
+
+finances = {}
