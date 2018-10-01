6c052a079e5489d64dd73926a78d5a7cf2d70a0d
Brendan M Whitney <bwhitne1@uvm.edu>
2018-09-30 20:33:28 -0400

    Second test of ssh connection

--- test.py
+++ test.py
@@ -4,9 +4,10 @@
 # created on: 09-30-2018
 # filename: test.py
 # author: brendan
-# last modified: 09-30-2018 20:23
+# last modified: 09-30-2018 20:33
 #-------------------------------------------------------------------------------
 """
-File made to test the ssh connection
+File made to test the ssh connection, we need to test 1 more time. So here
+are some small edits.
 """
 
